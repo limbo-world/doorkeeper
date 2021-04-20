31969787cf5f406494df549456e07f9e3fd41a1b
Devil <935494657@qq.com>
2021-04-20 11:37:38 +0800

    fix 权限编辑页面 穿梭框数据bug

--- vue/src/views/permission/PermissionEdit.vue
+++ vue/src/views/permission/PermissionEdit.vue
@@ -27,43 +27,13 @@
 
                 <el-form-item label="资源">
                     <el-container class="permission-resource-edit-page">
-                        <el-header height="30px">
-                            <el-form :inline="true" size="mini">
-                                <el-form-item label="名称">
-                                    <el-input v-model="resourceQueryForm.dimName" placeholder="输入名称"></el-input>
-                                </el-form-item>
-                                <el-form-item>
-                                    <el-button type="primary" @click="loadResources" size="mini" icon="el-icon-search">
-                                        查询
-                                    </el-button>
-                                </el-form-item>
-                            </el-form>
-                        </el-header>
-                        <el-main>
-                            <el-transfer v-model="selectResourceIds" :data="resources" :titles="['未绑定', '已绑定']"
-                                         @change="changeResource"></el-transfer>
-                        </el-main>
+                        <el-transfer v-model="selectResourceIds" :data="resources" :titles="['未绑定', '已绑定']"></el-transfer>
                     </el-container>
                 </el-form-item>
 
                 <el-form-item label="策略">
                     <el-container class="permission-policy-edit-page">
-                        <el-header height="30px">
-                            <el-form :inline="true" size="mini">
-                                <el-form-item label="名称">
-                                    <el-input v-model="policyQueryForm.dimName" placeholder="输入名称"></el-input>
-                                </el-form-item>
-                                <el-form-item>
-                                    <el-button type="primary" @click="loadPolicies" size="mini" icon="el-icon-search">
-                                        查询
-                                    </el-button>
-                                </el-form-item>
-                            </el-form>
-                        </el-header>
-                        <el-main>
-                            <el-transfer v-model="selectPolicyIds" :data="policies" :titles="['未绑定', '已绑定']"
-                                         @change="changePolicy"></el-transfer>
-                        </el-main>
+                        <el-transfer v-model="selectPolicyIds" :data="policies" :titles="['未绑定', '已绑定']"></el-transfer>
                     </el-container>
                 </el-form-item>
 
@@ -107,16 +77,6 @@ export default {
                 policies: []
             },
 
-            resourceQueryForm: {
-                name: '',
-                size: 100
-            },
-
-            policyQueryForm: {
-                name: '',
-                size: 100
-            },
-
             resources: [],
             selectResourceIds: [],
 
@@ -132,6 +92,8 @@ export default {
     created() {
         pages.permissionEdit = this;
         this.permission.clientId = this.$route.query.clientId;
+        this.loadResources();
+        this.loadPolicies();
         if (this.$route.query.permissionId) {
             this.permission.permissionId = this.$route.query.permissionId;
             this.loadPermission();
@@ -144,73 +106,29 @@ export default {
         // ========== 资源相关  ==========
         loadResources() {
             this.$ajax.get(`/admin/realm/${this.user.realm.realmId}/client/${this.permission.clientId}/resource`, {
-                params: this.resourceQueryForm
+                params: {needAll: true}
             }).then(response => {
                 let resources = response.data.data;
-                let resourceIds = [];
-                for (let resource of resources) {
+                resources.forEach(resource => {
                     resource.key = resource.resourceId;
                     resource.label = resource.name;
-                    resourceIds.push(resource.resourceId);
-                }
-                this.concatResources(resources, resourceIds);
+                })
+                this.resources = resources;
             });
-        },
-        changeResource(resourceIds, d, mvResourceIds) {
-            if (d === 'right') {
-                // 主要用于去除重复
-                this.permission.resources = this.permission.resources.filter(resource => mvResourceIds.indexOf(resource.resourceId) < 0)
-                // 获取新增的
-                let resources = this.resources.filter(resource => mvResourceIds.indexOf(resource.resourceId) >= 0)
-                this.permission.resources = this.permission.resources.concat(resources)
-            } else {
-                // 删除里面的
-                this.permission.resources = this.permission.resources.filter(resource => mvResourceIds.indexOf(resource.resourceId) < 0)
-            }
-        },
-        concatResources(resources, resourceIds) {
-            // 把不存在的加入进来
-            let permissionResources = this.permission.resources.filter(resource => resourceIds.indexOf(resource.resourceId) < 0)
-            resources = resources.concat(permissionResources);
-            this.resources = resources;
-            // 每次查询完，需要把已有的加入
-            this.selectResourceIds = this.permission.resources.map(resource => resource.resourceId)
         },
 
         // ========== 策略相关  ==========
         loadPolicies() {
             this.$ajax.get(`/admin/realm/${this.user.realm.realmId}/client/${this.permission.clientId}/policy`, {
-                params: this.policyQueryForm
+                params: {needAll: true}
             }).then(response => {
                 let policies = response.data.data;
-                let policyIds = [];
                 for (let policy of policies) {
                     policy.key = policy.policyId;
                     policy.label = policy.name;
-                    policyIds.push(policy.policyId);
                 }
-                this.concatPolicies(policies, policyIds);
+                this.policies = policies;
             });
-        },
-        changePolicy(policyIds, d, mvPolicyIds) {
-            if (d === 'right') {
-                // 主要用于去除重复
-                this.permission.policies = this.permission.policies.filter(policy => mvPolicyIds.indexOf(policy.policyId) < 0)
-                // 获取新增的
-                let policies = this.policies.filter(policy => mvPolicyIds.indexOf(policy.policyId) >= 0)
-                this.permission.policies = this.permission.policies.concat(policies)
-            } else {
-                // 删除里面的
-                this.permission.policies = this.permission.policies.filter(policy => mvPolicyIds.indexOf(policy.policyId) < 0)
-            }
-        },
-        concatPolicies(policies, policyIds) {
-            // 把不存在的加入进来
-            let permissionPolicies = this.permission.policies.filter(policy => policyIds.indexOf(policy.policyId) < 0)
-            policies = policies.concat(permissionPolicies);
-            this.policies = policies;
-            // 每次查询完，需要把已有的加入
-            this.selectPolicyIds = this.permission.policies.map(policy => policy.policyId)
         },
 
         // ========== 权限相关 ==========
@@ -218,22 +136,12 @@ export default {
             this.startProgress({speed: 'fast'});
             this.$ajax.get(`/admin/realm/${this.user.realm.realmId}/client/${this.permission.clientId}/permission/${this.permission.permissionId}`).then(response => {
                 this.permission = response.data;
-                this.loadResources();
-                this.loadPolicies();
+                this.selectPolicyIds = this.permission.policies.map(policy => policy.policyId)
+                this.selectResourceIds = this.permission.resources.map(resource => resource.resourceId)
             }).finally(() => this.stopProgress());
         },
         addPermission() {
-            let addParam = {...this.permission, resourceIds: [], policyIds: []}
-            if (addParam.resources && addParam.resources.length > 0) {
-                addParam.resources.forEach(r => {
-                    addParam.resourceIds.push(r.resourceId);
-                })
-            }
-            if (addParam.policies && addParam.policies.length > 0) {
-                addParam.policies.forEach(p => {
-                    addParam.policyIds.push(p.policyId);
-                })
-            }
+            let addParam = {...this.permission, resourceIds: this.selectResourceIds, policyIds: this.selectPolicyIds}
             this.$ajax.post(`/admin/realm/${this.user.realm.realmId}/client/${this.permission.clientId}/permission`, addParam).then(response => {
                 this.permission = response.data;
                 this.$message.success("新增成功")
@@ -241,17 +149,7 @@ export default {
             })
         },
         updatePermission() {
-            let updateParam = {...this.permission, resourceIds: [], policyIds: []}
-            if (updateParam.resources && updateParam.resources.length > 0) {
-                updateParam.resources.forEach(r => {
-                    updateParam.resourceIds.push(r.resourceId);
-                })
-            }
-            if (updateParam.policies && updateParam.policies.length > 0) {
-                updateParam.policies.forEach(p => {
-                    updateParam.policyIds.push(p.policyId);
-                })
-            }
+            let updateParam = {...this.permission, resourceIds: this.selectResourceIds, policyIds: this.selectPolicyIds}
             this.$ajax.put(`/admin/realm/${this.user.realm.realmId}/client/${this.permission.clientId}/permission/${this.permission.permissionId}`, updateParam).then(response => {
                 this.$message.success("更新成功")
                 this.loadPermission();
