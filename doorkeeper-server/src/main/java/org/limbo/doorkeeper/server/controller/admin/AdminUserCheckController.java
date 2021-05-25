f2c293d20c9cb5ed6297f7fd2d1f9c11d9fce4d2
Devil <935494657@qq.com>
2021-05-25 10:14:49 +0800

    check接口

--- doorkeeper-server/src/main/java/org/limbo/doorkeeper/server/controller/admin/AdminUserCheckController.java
+++ doorkeeper-server/src/main/java/org/limbo/doorkeeper/server/controller/admin/AdminUserCheckController.java
@@ -20,11 +20,19 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.limbo.doorkeeper.api.model.Response;
+import org.limbo.doorkeeper.api.model.param.check.GroupCheckParam;
 import org.limbo.doorkeeper.api.model.param.check.ResourceCheckParam;
+import org.limbo.doorkeeper.api.model.param.check.RoleCheckParam;
+import org.limbo.doorkeeper.api.model.vo.GroupVO;
 import org.limbo.doorkeeper.api.model.vo.ResourceVO;
+import org.limbo.doorkeeper.api.model.vo.RoleVO;
+import org.limbo.doorkeeper.api.model.vo.check.GroupCheckResult;
 import org.limbo.doorkeeper.api.model.vo.check.ResourceCheckResult;
+import org.limbo.doorkeeper.api.model.vo.check.RoleCheckResult;
 import org.limbo.doorkeeper.server.controller.BaseController;
+import org.limbo.doorkeeper.server.support.auth.GroupChecker;
 import org.limbo.doorkeeper.server.support.auth.ResourceChecker;
+import org.limbo.doorkeeper.server.support.auth.RoleChecker;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -44,10 +52,16 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/api/admin/realm/{realmId}/user/{userId}")
-public class AdminUserResourceController extends BaseController {
+public class AdminUserCheckController extends BaseController {
 
     @Autowired
     private ResourceChecker resourceChecker;
+
+    @Autowired
+    private RoleChecker roleChecker;
+
+    @Autowired
+    private GroupChecker groupChecker;
 
     @Operation(summary = "用户是否可以访问的资源")
     @GetMapping("/resource")
@@ -55,6 +69,20 @@ public class AdminUserResourceController extends BaseController {
                                                     @ParameterObject @Validated ResourceCheckParam param) {
         ResourceCheckResult check = resourceChecker.check(userId, true, param);
         return Response.success(check.getResources());
+    }
+
+    @Operation(summary = "检查用户拥有的角色")
+    @GetMapping("/role")
+    public Response<List<RoleVO>> checkRole(@ParameterObject @Validated RoleCheckParam param) {
+        RoleCheckResult result = roleChecker.check(getUser().getUserId(), param);
+        return Response.success(result.getRoles());
+    }
+
+    @Operation(summary = "检查用户所在的组")
+    @GetMapping("/group")
+    public Response<List<GroupVO>> checkGroup(@ParameterObject @Validated GroupCheckParam param) {
+        GroupCheckResult result = groupChecker.check(getUser().getUserId(), param);
+        return Response.success(result.getGroups());
     }
 
 }
